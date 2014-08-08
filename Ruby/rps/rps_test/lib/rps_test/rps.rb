a1d2aba5d5e5c4568de33bc62e7f345667712ea8
mntj <mntj11@gmail.com>
2014-08-08 18:29:07 -0400

    C - add dynamite move and handling to rps.rb

--- Ruby/rps/rps_test/lib/rps_test/rps.rb
+++ Ruby/rps/rps_test/lib/rps_test/rps.rb
@@ -3,23 +3,28 @@ class RPS
   def initialize
     @score, @computer_score, @wins, @losses = 0, 0, 0, 0
     @moves = ['rock', 'paper', 'scissors']
+    @dynamite_moves = @moves << 'dynamite'
   end
-  def play(move)
+  def play(move, dynamite=false)
     @player_move = move
-    @computer_move ||= @moves.sample
+    if dynamite
+      @computer_move ||= @dynamite_moves.sample
+    else
+      @computer_move ||= @moves.sample
+    end
     case @player_move
     when 'rock'
       if @computer_move === 'scissors'
         @score += 1
         @string = "Rock against scissors! You win!"
-        end_of_round?
+        end_of_round_checker
       elsif @computer_move === 'paper'
         @computer_score += 1
         @string = "Rock against paper! You lose!"
-        end_of_round?
+        end_of_round_checker
       elsif @computer_move === 'rock'
         @string = "Rock against rock! It's a tie!"
-        end_of_round?
+        end_of_round_checker
       else
         raise "I'm broken!"
       end
@@ -27,14 +32,14 @@ class RPS
       if @computer_move === 'rock'
         @score += 1
         @string = "Paper against rock! You win!"
-        end_of_round?
+        end_of_round_checker
       elsif @computer_move === 'scissors'
         @computer_score += 1
         @string = "Paper against scissors! You lose!"
-        end_of_round?
+        end_of_round_checker
       elsif @computer_move === 'paper'
         @string = "Paper against paper! It's a tie!"
-        end_of_round?
+        end_of_round_checker
       else
         raise "I'm broken!"
       end
@@ -42,20 +47,40 @@ class RPS
       if @computer_move === 'paper'
         @score += 1
         @string = "Scissors against paper! You win!"
-        end_of_round?
+        end_of_round_checker
       elsif @computer_move === 'rock'
         @computer_score += 1
         @string = "Scissors against rock! You lose!"
-        end_of_round?
+        end_of_round_checker
       elsif @computer_move === 'scissors'
         @string = "Scissors against scissors! It's a tie!"
-        end_of_round?
+        end_of_round_checker
       else
         raise "I'm broken!"
       end
+    when 'dynamite'
+      if dynamite
+        if @computer_move === 'paper'
+          @score += 1
+          @string = "Dynamite against paper! You win!"
+          end_of_round_checker
+        elsif @computer_move === 'rock'
+          @score += 1
+          @string = "Dynamite against rock! You win!"
+          end_of_round_checker
+        elsif @computer_move === 'scissors'
+          @computer_score += 1
+          @string = "Dynamite against scissors! You lose!"
+          end_of_round_checker
+        elsif @computer_move === 'dynamite'
+          @string = "Dynamite against dynamite! It's a tie!"
+          end_of_round_checker
+        end
+      else
+        raise "This isn't a dynamite game! Try play([your_move], true)"
     end
   end
-  def end_of_round?
+  def end_of_round_checker
     @computer_move = nil
     if @score === 3
       @score = 0
