a7758cd4419487c744ead09cbd162239bd90d5cc
mntj <mntj11@gmail.com>
2014-08-17 19:22:58 -0400

    C - add handling for additional move combinations enabled by lizard spock and dynamite moves

--- Ruby/rps/rps_test/lib/rps_test/rps.rb
+++ Ruby/rps/rps_test/lib/rps_test/rps.rb
@@ -1,16 +1,20 @@
 class RPS
   attr_reader :score, :computer_score, :wins, :losses
   def initialize(dynamite=false, lizard_spock=false)
+    @dynamite, @lizard_spock = dynamite, lizard_spock
     @score, @computer_score, @wins, @losses = 0, 0, 0, 0
     @moves = ['rock', 'paper', 'scissors']
-    @dynamite_moves = @moves << 'dynamite'
+    @dynamite_moves = @moves + ['dynamite']
     @lizard_spock_moves = @moves + ['lizard', 'spock']
+    @lizard_spock_dynamite = @lizard_spock_moves + ['dynamite']
   end
   def play(move)
     @player_move = move
-    if dynamite
+    if @dynamite && @lizard_spock
+      @computer_move ||= @lizard_spock_dynamite.sample
+    elsif @dynamite
       @computer_move ||= @dynamite_moves.sample
-    elsif lizard_spock
+    elsif @lizard_spock
       @computer_move ||= @lizard_spock_moves.sample
     else
       @computer_move ||= @moves.sample
@@ -28,6 +32,18 @@ class RPS
       elsif @computer_move === 'rock'
         @string = "Rock against rock! It's a tie!"
         end_of_round_checker
+      elsif @computer_move === 'dynamite'
+        @computer_score += 1
+        @string = "Rock against dynamite! You lose!"
+        end_of_round_checker
+      elsif @computer_move === 'spock'
+        @computer_score += 1
+        @string = "Rock against spock! You lose!"
+        end_of_round_checker
+      elsif @computer_move === 'lizard'
+        @score += 1
+        @string = "Rock against lizard! You win!"
+        end_of_round_checker
       else
         raise "I'm broken!"
       end
@@ -42,6 +58,18 @@ class RPS
         end_of_round_checker
       elsif @computer_move === 'paper'
         @string = "Paper against paper! It's a tie!"
+        end_of_round_checker
+      elsif @computer_move === 'dynamite'
+        @computer_score += 1
+        @string = "Paper against dynamite! You lose!"
+        end_of_round_checker
+      elsif @computer_move === 'spock'
+        @score += 1
+        @string = "Paper against spock! You win!"
+        end_of_round_checker
+      elsif @computer_move === 'lizard'
+        @computer_score += 1
+        @string = "Paper against lizard! You lose!"
         end_of_round_checker
       else
         raise "I'm broken!"
@@ -58,11 +86,23 @@ class RPS
       elsif @computer_move === 'scissors'
         @string = "Scissors against scissors! It's a tie!"
         end_of_round_checker
+      elsif @computer_move === 'dynamite'
+        @score += 1
+        @string = "Scissors against dynamite! You win!"
+        end_of_round_checker
+      elsif @computer_move === 'spock'
+        @computer_score += 1
+        @string = "Scissors against spock! You lose!"
+        end_of_round_checker
+      elsif @computer_move === 'lizard'
+        @score += 1
+        @string = "Scissors against lizard! You win!"
+        end_of_round_checker
       else
         raise "I'm broken!"
       end
     when 'dynamite'
-      if dynamite
+      if @dynamite
         if @computer_move === 'paper'
           @score += 1
           @string = "Dynamite against paper! You win!"
@@ -78,6 +118,12 @@ class RPS
         elsif @computer_move === 'dynamite'
           @string = "Dynamite against dynamite! It's a tie!"
           end_of_round_checker
+        elsif @computer_move === 'spock'
+          @string = "Dynamite against..spock?? What is this?"
+          end_of_round_checker
+        elsif @computer_move === 'lizard'
+          @string = "Dynamite against..lizard?? What is this?"
+          end_of_round_checker
         else
           raise "I'm broken!"
         end
@@ -85,7 +131,7 @@ class RPS
         raise "This isn't a dynamite game! Try RPS.new(true, false) to begin a dynamite game."
       end
     when 'lizard'
-      if lizard_spock
+      if @lizard_spock
         if @computer_move === 'paper'
           @score += 1
           @string = "Lizard against paper! You win!"
@@ -105,6 +151,9 @@ class RPS
         elsif @computer_move === 'lizard'
           @string = "Lizard against lizard! It's a tie!"
           end_of_round_checker
+        elsif @computer_move === 'dynamite'
+          @string = "Lizard against..dynamite?? What is this?"
+          end_of_round_checker
         else
           raise "I'm broken!"
         end
@@ -112,7 +161,7 @@ class RPS
         raise "This isn't a lizard-spock game! Try RPS.new(false, true) to begin a lizard-spock game."
       end
     when 'spock'
-      if lizard_spock
+      if @lizard_spock
         if @computer_move === 'paper'
           @computer_score += 1
           @string = "Spock against paper! You lose!"
@@ -132,6 +181,9 @@ class RPS
         elsif @computer_move === 'spock'
           @string = "Spock against spock! It's a tie!"
           end_of_round_checker
+        elsif @computer_move === 'dynamite'
+          @string = "Spock against dynamite! Nobody wins."
+          end_of_round_checker
         else
           raise "I'm broken!"
         end
@@ -147,16 +199,16 @@ class RPS
       @computer_score = 0
       @wins += 1
       puts @string
-      "You won the round!"
+      puts "You won the round!"
     elsif @computer_score === 3
       @score = 0
       @computer_score = 0
       @losses += 1
       puts @string
-      "You lost this round!"
+      puts "You lost this round!"
     else
       puts @string
-      "Your score: #{@score}, Computer score: #{@computer_score}"
+      puts "Your score: #{@score}, Computer score: #{@computer_score}"
     end
   end
 end
--- Ruby/rps/rps_test/lib/rps_test/version.rb
+++ Ruby/rps/rps_test/lib/rps_test/version.rb
@@ -1,3 +1,3 @@
 module RpsTest
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
