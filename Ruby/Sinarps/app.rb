1a34ed617a7d23493b808e1de6dae4d9598afc1d
mntj <mntj11@gmail.com>
2014-08-19 02:49:24 -0400

    C - RPS is functional using sessions

--- Ruby/Sinarps/app.rb
+++ Ruby/Sinarps/app.rb
@@ -1,6 +1,44 @@
 require 'bundler/setup'
 Bundler.require
+enable :sessions
 
 get '/' do
   erb :index
+end
+
+get '/rps' do
+  erb :rps
+end
+
+get '/reset' do
+  session.clear
+  redirect to('/')
+end
+
+get '/dynamite' do
+  d ||= RPS.new(true,false)
+  erb :dynamite
+end
+
+get '/lizard-spock' do
+  l ||= RPS.new(false,true)
+  erb :lizard_spock
+end
+
+get '/rps/:move' do
+  r = session[:game] || RPS.new
+  pass_vars_to_session(r)
+  redirect to('/rps')
+end
+
+def pass_vars_to_session(game)
+  session[:game] = game
+  session[:score_board] = game.play(params[:move])
+  session[:score] = game.score
+  session[:computer_score] = game.computer_score
+  session[:wins] = game.wins
+  session[:losses] = game.losses
+  session[:string] = game.instance_variable_get(:@string)
+  session[:player_move] = game.instance_variable_get(:@player_move)
+  session[:computer_move] = game.instance_variable_get(:@computer_move)
 end
